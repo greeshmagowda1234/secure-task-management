87ba24791aec2a74236243ae5848643e5dbcacbc
greeshmagowda1234 <gowdagreeshma463@gmail.com>
2026-05-06 19:38:15 +0530

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    email: req.body.email,
+    password: hashed
+  });
+
+  await user.save();
+  res.json({ message: "User registered" });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, "secretkey");
+  res.json({ token });
+});
+
+module.exports = router;
